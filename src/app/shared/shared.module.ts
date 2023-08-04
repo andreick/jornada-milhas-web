f69f00280c3a3aa13641b68e1d00777a97471f6f
andreick <andreick.gomes.ismart@gmail.com>
2023-08-03 22:09:42 -0300

    Cria e estiliza o header

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
+  exports: [
+    HeaderComponent,
+  ]
+})
+export class SharedModule { }
