9c12d3023335af888d162ccad025055fd3154495
andreick <andreick.gomes.ismart@gmail.com>
2023-08-05 00:47:32 -0300

    Cria card de busca

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { ContainerComponent } from './container/container.component';
 import { FooterComponent } from './footer/footer.component';
 import { CardComponent } from './card/card.component';
+import { CardSearchComponent } from './card-search/card-search.component';
 
 @NgModule({
   declarations: [
@@ -17,17 +19,21 @@ import { CardComponent } from './card/card.component';
     ContainerComponent,
     FooterComponent,
     CardComponent,
+    CardSearchComponent,
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ],
   exports: [
     HeaderComponent,
     BannerComponent,
     ContainerComponent,
     FooterComponent,
+    CardComponent,
+    CardSearchComponent,
   ]
 })
 export class SharedModule { }
