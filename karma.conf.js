3e791958b0b843c54519ad5eec33f94c12284872
andreick <andreick.gomes.ismart@gmail.com>
2023-08-13 14:07:14 -0300

    Renomeia a aplicação para jornada-milhas-web

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jornada-milhas'),
+      dir: require('path').join(__dirname, './coverage/jornada-milhas-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jornada-milhas'`, () => {
+  it(`should have as title 'jornada-milhas-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jornada-milhas');
+    expect(app.title).toEqual('jornada-milhas-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jornada-milhas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jornada-milhas-web app is running!');
   });
 });
