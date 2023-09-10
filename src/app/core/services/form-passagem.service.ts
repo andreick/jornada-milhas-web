1b4bbca11e7ba1a8f27582ea60ac891bd1a6af47
andreick <andreick.gomes.ismart@gmail.com>
2023-09-10 01:15:57 -0300

    Adiciona os passageiros no formPassagem

--- src/app/core/services/form-passagem.service.ts
+++ src/app/core/services/form-passagem.service.ts
@@ -12,11 +12,14 @@ import { Estado } from '../models/estado';
 })
 export class FormPassagemService {
 
-  readonly formPassagem: FormGroup;
+  readonly formPassagem;
   readonly somenteIda: FormControl<boolean | null>;
   readonly origem: FormControl<string | Estado | null>;
   readonly destino: FormControl<string | Estado | null>;
-  readonly categoria: FormControl<'economica' | 'executiva' | null>;
+  readonly categoria: FormControl<'Econômica' | 'Executiva' | null>;
+  readonly adultos: FormControl<number | null>;
+  readonly criancas: FormControl<number | null>;
+  readonly bebes: FormControl<number | null>;
 
   constructor(
     private dialog: MatDialog,
@@ -25,15 +28,34 @@ export class FormPassagemService {
       somenteIda: this.somenteIda = new FormControl(false),
       origem: this.origem = new FormControl(''),
       destino: this.destino = new FormControl(''),
-      categoria: this.categoria = new FormControl('executiva'),
+      categoria: this.categoria = new FormControl('Executiva'),
+      adultos: this.adultos = new FormControl(1),
+      criancas: this.criancas = new FormControl(0),
+      bebes: this.bebes = new FormControl(0),
     });
   }
 
-  openDialog() {
+  get descricaoPassageiros(): string {
+    const listaQuantidade = [
+      { quantidade: this.adultos.value ?? 0, singular: 'Adulto', plural: 'Adultos' },
+      { quantidade: this.criancas.value ?? 0, singular: 'Criança', plural: 'Crianças' },
+      { quantidade: this.bebes.value ?? 0, singular: 'Bebê', plural: 'Bebês' },
+    ];
+
+    return listaQuantidade
+      .filter(({ quantidade }) => quantidade > 0)
+      .map(({ quantidade, singular, plural }) => `${quantidade} ${quantidade > 1 ? plural : singular}`)
+      .reduce((descricao, passageiro, indice, lista) => {
+        if (!descricao) return passageiro;
+        return descricao += `${indice < lista.length - 1 ? ',' : ' e'} ${passageiro}`;
+      }, '');
+  }
+
+  openDialog(): void {
     this.dialog.open(ModalComponent, { width: '50%' });
   }
 
-  categoriaSelected(categoria: 'economica' | 'executiva'): void {
+  categoriaSelected(categoria: 'Econômica' | 'Executiva'): void {
     this.categoria.setValue(categoria);
   }
 
