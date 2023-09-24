e34fce7292cd9ac9f01d87ef9dad6cce9832f3e5
andreick <andreick.gomes.ismart@gmail.com>
2023-09-24 15:57:15 -0300

    Integra o contador de passageiros com o Angular Forms

--- src/app/shared/contador-passageiro/contador-passageiro.component.ts
+++ src/app/shared/contador-passageiro/contador-passageiro.component.ts
@@ -1,18 +1,63 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-contador-passageiro',
   templateUrl: './contador-passageiro.component.html',
-  styleUrls: ['./contador-passageiro.component.scss']
+  styleUrls: ['./contador-passageiro.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: ContadorPassageiroComponent
+    }
+  ]
 })
-export class ContadorPassageiroComponent implements OnInit {
+export class ContadorPassageiroComponent implements ControlValueAccessor {
 
   @Input() titulo: string = '';
   @Input() subtitulo: string = '';
 
+  contador: number = 0;
+
+  onChange = (contador: number) => { };
+
+  onTouched = () => { };
+
+  desabilitado: boolean = false;
+
   constructor() { }
 
-  ngOnInit(): void {
+  incrementar(): void {
+    if (!this.desabilitado) {
+      this.onTouched();
+      this.contador++;
+      this.onChange(this.contador);
+    }
+  }
+
+  decrementar(): void {
+    if (!this.desabilitado && this.contador > 0) {
+      this.onTouched();
+      this.contador--;
+      this.onChange(this.contador);
+    }
+  }
+
+  writeValue(contador: number): void {
+    this.contador = contador;
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState?(desabilitado: boolean): void {
+    this.desabilitado = desabilitado;
   }
 
 }
